c590fd81d187cc79f86a25a777dcdc6efd7f61ca
jfmolano1587 <jfm@JFMs-Mac-mini.local>
2016-09-17 14:45:19 -0500

    Titulo y DOB no parseado
    .

--- WordCountMR/src/uniandes/mapRed/WCMapper.java
+++ WordCountMR/src/uniandes/mapRed/WCMapper.java
@@ -2,7 +2,8 @@ package uniandes.mapRed;
 
 import java.io.IOException;
 import java.util.HashMap;
-
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
@@ -28,9 +29,31 @@ public class WCMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
 							:1);
 		}*/
 		//for(String k:palabrasLinea.keySet()){
-		String val = value.toString();
+	    // Now create matcher object.
+		String val = value.toString();	    
 		if(val.contains("Nairo Quintana")){
-			context.write(new Text(val), new IntWritable(1));			
+			String pattern_infobox = "\\{\\{Infobox[\\s\\S]*?\\}\\}";
+		    Pattern p_infobox = Pattern.compile(pattern_infobox, Pattern.MULTILINE);
+			Matcher m_infobox = p_infobox.matcher(value.toString());
+		    String info_box = "";
+		    if (m_infobox.find()) {
+		    	info_box = m_infobox.group(0);
+				String pattern_dob = "birth_date[\\s\\S]*?\\}\\}";
+			    Pattern p_dob = Pattern.compile(pattern_dob, Pattern.MULTILINE);
+		    	Matcher m_dob = p_dob.matcher(info_box.toString());
+			    String dob = "";
+			    if (m_dob.find()) {
+			    	dob = m_dob.group(0);
+			    	String pattern_title = "<title>[\\s\\S]*?<\\/title>";
+				    Pattern p_title = Pattern.compile(pattern_title, Pattern.MULTILINE);
+			    	Matcher m_title = p_title.matcher(val.toString());
+				    String titulo = "";
+				    if (m_title.find()) {
+				    	titulo = m_title.group(0);
+						context.write(new Text(titulo+";"+dob), new IntWritable(1));
+				    }
+			    }
+		    }			
 		}
 		//}
 		
