a998a396a245cc1d3a55b3b3f346b81d2dc59226
jfmolano1587 <jfm@JFMs-Mac-mini.local>
2016-09-18 17:47:21 -0500

    jar de consulta

--- Busqueda/src/uniandes/mapRed/WCMapper.java
+++ Busqueda/src/uniandes/mapRed/WCMapper.java
@@ -1,6 +1,10 @@
 package uniandes.mapRed;
 
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -9,6 +13,7 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Mapper.Context;
 
 public class WCMapper extends Mapper<LongWritable, Text, Text, Text> {
 	
@@ -16,12 +21,87 @@ public class WCMapper extends Mapper<LongWritable, Text, Text, Text> {
 	protected void map(LongWritable key, Text value,
 			Context context)
 			throws IOException, InterruptedException {
-			String val = value.toString();
-			String [] arr_t = val.split(";");
-			String a = arr_t[1];
-			String b = arr_t[2];
-			context.write(new Text(a), new Text("sem;"+b));
-			context.write(new Text(a), new Text("join;"+b+";"+a));
-		}		
+		String fecha_ini_str = "1993-07-01";
+		String fecha_fin_str = "1993-07-30";
+		String pais_busqueda = "-";
+		String nombre_busqueda = "-";
+		Date fecha_ini = null;
+		Date fecha_fin = null;
+		try{
+			DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+			fecha_ini = df.parse(fecha_ini_str);
+			fecha_fin = df.parse(fecha_fin_str);
+		}catch(Exception e){
+			
+		}
+		
+		String val = value.toString();
+		if(val.startsWith("P")){
+			boolean cumple_fecha = false;
+			boolean cumple_nombre = false;
+			boolean cumple_pais = false;
+			String nombre = "EMPTY_STR";
+			String fecha = "EMPTY_STR";
+			String pais = "EMPTY_STR";
+			String[] arr = val.split(";");
+			try{
+				nombre = arr[1];
+				fecha = arr[2];
+				pais = arr[3];				
+			}catch(Exception e){
+				
+			}
+			if(fecha_ini_str.equals("-")){
+				cumple_fecha = true;
+			}else{
+				try{
+					DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+					Date bDate = df.parse(fecha);
+					if(bDate.before(fecha_fin) && bDate.after(fecha_ini)){
+						cumple_fecha = true;
+					}
+				}catch(ArrayIndexOutOfBoundsException | ParseException e){
+					
+				}
+			}
+			
+			if(nombre_busqueda.equals("-")){
+				cumple_nombre = true;
+			}else{
+				if(nombre.contains(nombre_busqueda)){
+					cumple_nombre = true;
+				}
+			}
+			
+			if(pais_busqueda.equals("-")){
+				cumple_pais = true;
+			}else{
+				if(pais.contains(pais_busqueda)){
+					cumple_pais = true;
+				}
+			}
+			if(cumple_fecha && cumple_nombre && cumple_pais){
+				context.write(new Text(nombre), new Text("P"+nombre+";"+fecha+";"+pais+";"));				
+			}
+		}else if(val.startsWith("LR")){
+			String[] arr = val.split(";");
+			try{
+				String nombre = arr[1];
+				context.write(new Text(nombre), new Text("LI"+arr[1]+";"+arr[2]));					
+			}catch(ArrayIndexOutOfBoundsException e){
+				
+			}
+		}else if(val.startsWith("LD")){
+			String[] arr = val.split(";");
+			try{
+				String nombre = arr[1];
+				context.write(new Text(nombre), new Text("LI"+arr[2]+";"+arr[3]));					
+			}catch(ArrayIndexOutOfBoundsException e){
+				
+			}	
+		}
 	}
+}
+
+
 
--- Busqueda/src/uniandes/mapRed/WCReducer.java
+++ Busqueda/src/uniandes/mapRed/WCReducer.java
@@ -12,25 +12,24 @@ public class WCReducer extends Reducer<Text, Text, Text, IntWritable> {
 	protected void reduce(Text key, Iterable<Text> values,
 			Context context)
 			throws IOException, InterruptedException {
-		ArrayList<String> semillas = new ArrayList<String>();
-		ArrayList<String> joins = new ArrayList<String>();
+		ArrayList<String> links = new ArrayList<String>();
+		String respuesta = "";
+		boolean escribir = false;
 		for(Text iw:values){
 			String val = iw.toString();
 			String[] val_arr = val.split(";");
-			if(val_arr[0].equals("sem")){
-				semillas.add(val_arr[1]);
+			if(val_arr[0].equals("P")){
+				escribir = true;
+				respuesta = val;
 			}else{
-				joins.add(val_arr[1]+";"+val_arr[2]);
+				links.add(val);
 			}
 		}
-		for(int i = 0;i<joins.size();i++){
-			String join_str = joins.get(i);
-			for(int j = 0;j<semillas.size();j++){
-				String semilla_str = semillas.get(j);
-				if(!join_str.contains(semilla_str)){
-					context.write(new Text("LD;"+join_str+";"+semilla_str+";"), new IntWritable(0));					
-				}
+		if(escribir){
+			for(int i=0;i<links.size();i++){
+				respuesta = respuesta + links.get(i);
 			}
+			context.write(new Text(respuesta), new IntWritable(1));
 		}
 	}
 
