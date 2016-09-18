3e370ac1fdca4d01bae35c4131c2a0e899440c90
jfmolano1587 <jfm@JFMs-Mac-mini.local>
2016-09-18 01:36:27 -0500

    Arbol Nivel 2
    .

---
+++ LinksDoblesCuentas/src/uniandes/mapRed/WCMapper.java
@@ -0,0 +1,27 @@
+package uniandes.mapRed;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class WCMapper extends Mapper<LongWritable, Text, Text, Text> {
+	
+	@Override
+	protected void map(LongWritable key, Text value,
+			Context context)
+			throws IOException, InterruptedException {
+			String val = value.toString();
+			String [] arr_t = val.split(";");
+			String a = arr_t[1];
+			String b = arr_t[2];
+			context.write(new Text(a), new Text("sem;"+b));
+			context.write(new Text(a), new Text("join;"+b+";"+a));
+		}		
+	}
+
