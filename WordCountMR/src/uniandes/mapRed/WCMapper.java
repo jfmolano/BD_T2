9b9c1aea303b008b59bb51d10624bcf6200da661
jfmolano1587 <jfm@JFMs-Mac-mini.local>
2016-09-17 19:17:13 -0500

    Fecha de nacimiento, lugar de nacimiento y nombre
    .

--- WordCountMR/src/uniandes/mapRed/WCMapper.java
+++ WordCountMR/src/uniandes/mapRed/WCMapper.java
@@ -31,7 +31,7 @@ public class WCMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
 		//for(String k:palabrasLinea.keySet()){
 	    // Now create matcher object.
 		String val = value.toString();	    
-		if(val.contains("Radamel Falcao")){
+		if(val.contains("Barack Obama")){
 			String pattern_infobox = "\\{\\{Infobox[\\s\\S]*?\\}\\}";
 		    Pattern p_infobox = Pattern.compile(pattern_infobox, Pattern.MULTILINE);
 			Matcher m_infobox = p_infobox.matcher(value.toString());
@@ -40,7 +40,7 @@ public class WCMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
 		    	info_box = m_infobox.group(0);
 				String pattern_dob = "birth_date[\\s\\S]*?\\}\\}";
 			    Pattern p_dob = Pattern.compile(pattern_dob, Pattern.MULTILINE);
-		    	Matcher m_dob = p_dob.matcher(info_box.toString());
+		    	Matcher m_dob = p_dob.matcher(info_box);
 			    String dob = "";
 			    if (m_dob.find()) {
 			    	dob = m_dob.group(0);
@@ -62,12 +62,27 @@ public class WCMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
 				    	//Titulo
 				    	String pattern_title = "<title>[\\s\\S]*?<\\/title>";
 					    Pattern p_title = Pattern.compile(pattern_title, Pattern.MULTILINE);
-				    	Matcher m_title = p_title.matcher(val.toString());
+				    	Matcher m_title = p_title.matcher(val);
 					    String titulo = "";
+					    //POB
+					    String pattern_pob = "birth_place[\\s\\S]*?\\|";
+					    Pattern p_pob = Pattern.compile(pattern_pob, Pattern.MULTILINE);
+				    	Matcher m_pob = p_pob.matcher(val);
+				    	String pob = "";
+					    if (m_pob.find()) {
+					    	pob = m_pob.group(0).split("=")[1];
+					    	pob = pob.replace("\n", "");
+					    	pob = pob.replace("\r", "");
+					    	pob = pob.replace(" ", "");
+					    	pob = pob.replace("[", "");
+					    	pob = pob.replace("]", "");
+					    	pob = pob.substring(0,pob.length()-1);
+					    }
 					    if (m_title.find()) {
 					    	titulo = m_title.group(0);
 							titulo = titulo.substring(7, titulo.length()-8);
-							context.write(new Text(titulo+";"+fecha), new IntWritable(1));
+							context.write(new Text(titulo+";"+fecha+";"+pob), new IntWritable(1));
+							context.write(new Text("Cuenta: "), new IntWritable(1));
 					    }
 				    }
 				    catch(Exception e){
