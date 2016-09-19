b2a97f1b9c8dffbfea02e0107602711164c15c58
jfmolano1587 <jose.francisco.molano@gmail.com>
2016-09-19 09:20:07 -0500

    obtener resultados de cluster

--- conexion_servidor_hadoop/conexion.py
+++ conexion_servidor_hadoop/conexion.py
@@ -1,9 +1,23 @@
 import paramiko
 
+
+#ip = 'IPFALSA'
+#usuario = 'USUARIO_FALSO'
+#password_cluster = 'PASS FALSO'
+#ruta = 'RUTA FALSA'
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(
     paramiko.AutoAddPolicy())
-ssh.connect('IP_FALSA', username='USUARIO_FALSO',
-    password='PASSWORD_FALSO')
-stdin, stdout, stderr = ssh.exec_command('hadoop jar busqueda_parametros.jar uniandes.reuters.job.WordCounter /RUTA_FALSA/arbol_N2_V3/* /RUTA_FALSA/resultado_parametros Balvin Colombia - 1960-01-01')
-stdout.readlines()
+ssh.connect(ip, username=usuario,
+    password=password_cluster)
+stdin, stdout, stderr = ssh.exec_command('hadoop fs -rm -r /'+ruta+'/resultado_parametros/')
+salida = stdout.readlines()
+print salida
+stdin, stdout, stderr = ssh.exec_command('hadoop jar busqueda_parametros.jar uniandes.reuters.job.WordCounter /'+ruta+'/arbol_N2_V3/* /'+ruta+'/resultado_parametros Balvin Colombia - 1960-01-01')
+salida = stdout.readlines()
+print salida
+stdin, stdout, stderr = ssh.exec_command('hadoop fs -cat /'+ruta+'/resultado_parametros/*')
+out = stdout.readlines()
+for el in out:
+	print el
+#print out
